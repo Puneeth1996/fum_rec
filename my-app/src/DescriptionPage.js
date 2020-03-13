363f5e28e6e6a46bda2bd398202cb61da5091f32
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-13 11:56:04 +0530

    making of the description page

--- my-app/src/DescriptionPage.js
+++ my-app/src/DescriptionPage.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 
 const DescriptionPage = ({ match, location }) => {
     const {
-      params: { description }
+        params: { description }
     } = match;
-  
-    return (
-      <>
-        <p>
-          <strong>User ID: </strong>
-          {description}
-        </p>
-        
-      </>
-    );
-  };
 
-export default DescriptionPage;
+    return (
+        <>
+            <Link to="/"><h2 className="center ">Back to home</h2></Link>
+            <p><strong>User ID: </strong>{description}</p>
+        </>
+    );
+};
+
+export default connect()(JobLink);
