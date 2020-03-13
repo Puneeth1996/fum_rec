604d9c52eaeaf1dcd98b913e54a595940eb201d6
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-13 12:17:15 +0530

    Working code

--- my-app/src/DescriptionPage.js
+++ my-app/src/DescriptionPage.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
+import Post from './Post';
 
-const DescriptionPage = ({ match, location }) => {
-    const {
-        params: { description }
-    } = match;
+class DescriptionPage extends Component {
+    render() {
+        console.log(this.props.posts);
+        return (
+            <>
+                <Link to="/"><h2 className="center ">Back to home</h2></Link>
+                <p><strong>User ID: </strong>{this.props.match.params.description}</p>
+                {this.props.posts.map((post) => (post.title === this.props.match.params.description) && <Post post={post} key={post.id} />)}
+                    
 
-    return (
-        <>
-            <Link to="/"><h2 className="center ">Back to home</h2></Link>
-            <p><strong>User ID: </strong>{description}</p>
-        </>
-    );
-};
+            </>
+        );
+    }
+}
 
-export default connect()(JobLink);
+const mapStateToProps = (state) => {
+    return {
+    posts: state
+    }
+}
+export default connect(mapStateToProps)(DescriptionPage);
