448aebdb69ff3163f39997b6ce7b0ad7225f0760
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-13 11:17:21 +0530

    making of the routing with redux implemation soon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+
+
+const users = [
+  {
+    name: `Param`
+  },
+  {
+    name: `Vennila`
+  },
+  {
+    name: `Afrin`
+  }
+];
+
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
+};
+
+const AboutPage = () => {
+  return <h3>About Page</h3>;
+};
+
+const UsersPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {users.map((user, index) => (
+        <h5 key={index}>
+          <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+        </h5>
+      ))}
+    </>
   );
-}
+};
 
-export default App;
+const UserPage = ({ match, location }) => {
+  const {
+    params: { userId }
+  } = match;
+
+  return (
+    <>
+      <p>
+        <strong>User ID: </strong>
+        {userId}
+      </p>
+      <p>
+        <strong>User Name: </strong>
+        {users[userId - 1].name}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <section className="App">
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Route exact path="/" component={IndexPage} />
+        <Route exact path="/users" component={UsersPage} />
+        <Route exact path="/user/:userId" component={UserPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Router>
+      <a href="/about">about  |  with browser reload</a>
+    </section>
+  );
+};
