6462e0b33e95bf5afa9ecef5c851d6454f365cd7
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-13 15:56:29 +0530

    working making loacal storage

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,30 @@ import "./index.css";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import postReducer from './reducers/postReducer';
+import {loadState,saveState} from './localStorage'
+import throttle from 'lodash/throttle';
 
 import App from './App';
 
+const persistedState = loadState();
 
-const store = createStore(postReducer);
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const store = createStore(
+    postReducer,
+    persistedState
+);
+
+
+store.subscribe(throttle(() => {
+    saveState({
+        postReducer: store.getState().postReducer
+    });
+}, 1000));
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,24 +1,8 @@
 
 
 
-const persistedState = loadState();
-const store = createStore(
-    app,
-    persistedState
-);
-store.subscribe(() => {
-    saveState({
-        todos: store.getState().todos
-    });
-});
 
 
 
 
-import throttle from 'lodash.throttle';
 
-store.subscribe(throttle(() => {
-    saveState({
-        todos: store.getState().todos
-    });
-}, 1000));
